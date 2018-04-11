0a9defab50381782d629ca675b875bd256e9fecf
goodorra <35979679+goodorra@users.noreply.github.com>
2018-04-10 23:03:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// A $// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    //when I click to find out 
+    //reveal more information
+
+    $("a").on("click", function (){ 
+    	$(".col").append("<h1>WORLD PEACE</h1>")
+
+    });
+
+
+     });
